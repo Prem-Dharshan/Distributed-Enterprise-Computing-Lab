df3c1f0d9dfe588e8c54d5f9dd63b60a27a0412a
Prem Dharshan <whizzkid.dpd@gmail.com>
2025-02-28 23:03:20 +0530

    Ignorance is bliss

--- WS03/ticket_booking/booking/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Event, Show, Booking  # Import your models
-
-# Register your models here
-admin.site.register(Event)
-admin.site.register(Show)
-admin.site.register(Booking)
